feabe93c45b775371faf5322a6e4b93d427409fd
JayMehtaJBM <69752445+JayMehtaJBM@users.noreply.github.com>
2020-10-08 12:59:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,401 @@
+function c() {
+  var c = new Audio("sound/c.mp3");
+  c.play();
+  $(".c").addClass("clicked");
+  setTimeout(function () {
+    $(".c").removeClass("clicked");
+  }, 100);
+}
+
+function c_s() {
+  var c_s = new Audio("sound/c-sharp.mp3");
+  c_s.play();
+  $(".c_s").addClass("clicked");
+  setTimeout(function () {
+    $(".c_s").removeClass("clicked");
+  }, 100);
+}
+
+function d() {
+  var d = new Audio("sound/d.mp3");
+  d.play();
+  $(".d").addClass("clicked");
+  setTimeout(function () {
+    $(".d").removeClass("clicked");
+  }, 100);
+}
+
+function d_s() {
+  var d_s = new Audio("sound/d-sharp.mp3");
+  d_s.play();
+  $(".d_s").addClass("clicked");
+  setTimeout(function () {
+    $(".d_s").removeClass("clicked");
+  }, 100);
+}
+
+function e() {
+  var e = new Audio("sound/e.mp3");
+  e.play();
+  $(".e").addClass("clicked");
+  setTimeout(function () {
+    $(".e").removeClass("clicked");
+  }, 100);
+}
+
+function f() {
+  var f = new Audio("sound/f.mp3");
+  f.play();
+  $(".f").addClass("clicked");
+  setTimeout(function () {
+    $(".f").removeClass("clicked");
+  }, 100);
+}
+
+function f_s() {
+  var f_s = new Audio("sound/f-sharp.mp3");
+  f_s.play();
+  $(".f_s").addClass("clicked");
+  setTimeout(function () {
+    $(".f_s").removeClass("clicked");
+  }, 100);
+}
+
+function g() {
+  var g = new Audio("sound/g.mp3");
+  g.play();
+  $(".g").addClass("clicked");
+  setTimeout(function () {
+    $(".g").removeClass("clicked");
+  }, 100);
+}
+
+function g_s() {
+  var g_s = new Audio("sound/g-sharp.mp3");
+  g_s.play();
+  $(".g_s").addClass("clicked");
+  setTimeout(function () {
+    $(".g_s").removeClass("clicked");
+  }, 100);
+}
+
+function a() {
+  var a = new Audio("sound/a.mp3");
+  a.play();
+  $(".a").addClass("clicked");
+  setTimeout(function () {
+    $(".a").removeClass("clicked");
+  }, 100);
+}
+
+function a_s() {
+  var a_s = new Audio("sound/a-sharp.mp3");
+  a_s.play();
+  $(".a_s").addClass("clicked");
+  setTimeout(function () {
+    $(".a_s").removeClass("clicked");
+  }, 100);
+}
+
+function b() {
+  var b = new Audio("sound/b.mp3");
+  b.play();
+  $(".b").addClass("clicked");
+  setTimeout(function () {
+    $(".b").removeClass("clicked");
+  }, 100);
+}
+
+function c_h() {
+  var c_h = new Audio("sound/high-c.mp3");
+  c_h.play();
+  $(".c_h").addClass("clicked");
+  setTimeout(function () {
+    $(".c_h").removeClass("clicked");
+  }, 100);
+}
+
+function c_h_s() {
+  var c_h_s = new Audio("sound/high-c-sharp.mp3");
+  c_h_s.play();
+  $(".c_h_s").addClass("clicked");
+  setTimeout(function () {
+    $(".c_h_s").removeClass("clicked");
+  }, 100);
+}
+
+function d_h() {
+  var d_h = new Audio("sound/high-d.mp3");
+  d_h.play();
+  $(".d_h").addClass("clicked");
+  setTimeout(function () {
+    $(".d_h").removeClass("clicked");
+  }, 100);
+}
+
+function d_h_s() {
+  var d_h = new Audio("sound/-d-sharp.mp3");
+  d_h.play();
+  $(".d_h_s").addClass("clicked");
+  setTimeout(function () {
+    $(".d_h_s").removeClass("clicked");
+  }, 100);
+}
+
+function e_h() {
+  var e_h = new Audio("sound/high-e.mp3");
+  e_h.play();
+  $(".e_h").addClass("clicked");
+  setTimeout(function () {
+    $(".e_h").removeClass("clicked");
+  }, 100);
+}
+
+function twinkle() {
+  setTimeout(function () {
+    c();
+  });
+  setTimeout(function () {
+    c();
+  }, 350);
+  setTimeout(function () {
+    g();
+  }, 700);
+  setTimeout(function () {
+    g();
+  }, 1050);
+  setTimeout(function () {
+    a();
+  }, 1400);
+  setTimeout(function () {
+    a();
+  }, 1750);
+  setTimeout(function () {
+    g();
+  }, 2100);
+  setTimeout(function () {
+    f();
+  }, 2800);
+  setTimeout(function () {
+    f();
+  }, 3150);
+  setTimeout(function () {
+    e();
+  }, 3400);
+  setTimeout(function () {
+    e();
+  }, 3750);
+  setTimeout(function () {
+    d();
+  }, 4100);
+  setTimeout(function () {
+    d();
+  }, 4450);
+  setTimeout(function () {
+    c();
+  }, 4800);
+  setTimeout(function () {
+    g();
+  }, 5500);
+  setTimeout(function () {
+    g();
+  }, 5850);
+  setTimeout(function () {
+    f();
+  }, 6200);
+  setTimeout(function () {
+    f();
+  }, 6500);
+  setTimeout(function () {
+    e();
+  }, 6900);
+  setTimeout(function () {
+    e();
+  }, 7250);
+  setTimeout(function () {
+    d();
+  }, 7600);
+  setTimeout(function () {
+    g();
+  }, 8300);
+  setTimeout(function () {
+    g();
+  }, 8650);
+  setTimeout(function () {
+    f();
+  }, 8900);
+  setTimeout(function () {
+    f();
+  }, 9350);
+  setTimeout(function () {
+    e();
+  }, 9700);
+  setTimeout(function () {
+    d();
+  }, 10050);
+  setTimeout(function () {
+    c();
+  }, 10400);
+}
+
+function hbd() {
+  setTimeout(function () {
+    c();
+  });
+  setTimeout(function () {
+    c();
+  }, 300);
+  setTimeout(function () {
+    d();
+  }, 500);
+  setTimeout(function () {
+    c();
+  }, 900);
+  setTimeout(function () {
+    f();
+  }, 1300);
+  setTimeout(function () {
+    e();
+  }, 1800);
+  setTimeout(function () {
+    c();
+  }, 2400);
+  setTimeout(function () {
+    c();
+  }, 2700);
+  setTimeout(function () {
+    d();
+  }, 2900);
+  setTimeout(function () {
+    c();
+  }, 3300);
+  setTimeout(function () {
+    g();
+  }, 3700);
+  setTimeout(function () {
+    f();
+  }, 4200);
+  setTimeout(function () {
+    c();
+  }, 4700);
+  setTimeout(function () {
+    c();
+  }, 5000);
+  setTimeout(function () {
+    c_h();
+  }, 5300);
+  setTimeout(function () {
+    a();
+  }, 5650);
+  setTimeout(function () {
+    f();
+  }, 6100);
+  setTimeout(function () {
+    e();
+  }, 6550);
+  setTimeout(function () {
+    d();
+  }, 6900);
+  setTimeout(function () {
+    a_s();
+  }, 7300);
+  setTimeout(function () {
+    a_s();
+  }, 7600);
+  setTimeout(function () {
+    a();
+  }, 7900);
+  setTimeout(function () {
+    f();
+  }, 8300);
+  setTimeout(function () {
+    g();
+  }, 8700);
+  setTimeout(function () {
+    f();
+  }, 9100);
+}
+
+function kd() {
+
+    setTimeout(function () {
+      c();
+    });
+    setTimeout(function () {
+      c();
+    }, 500);
+    setTimeout(function () {
+      g();
+    }, 800);
+    setTimeout(function () {
+      f();
+    }, 1100);
+    setTimeout(function () {
+      d_s();
+    }, 1600);
+    setTimeout(function () {
+      d();
+    }, 2100);
+    setTimeout(function () {
+      d();
+    }, 2600);
+    setTimeout(function () {
+      d_s();
+    }, 2900);
+    setTimeout(function () {
+      f();
+    }, 3200);
+    setTimeout(function () {
+      d_s();
+    }, 3700);
+    setTimeout(function () {
+      d();
+    }, 4000);
+    setTimeout(function () {
+      c();
+    }, 4300);
+    setTimeout(function () {
+      c();
+    }, 4800);
+    setTimeout(function () {
+      a_s();
+    }, 5100);
+    setTimeout(function () {
+      a();
+    }, 5400);
+    setTimeout(function () {
+      a_s();
+    }, 5700);
+    setTimeout(function () {
+      a();
+    }, 6000);
+    setTimeout(function () {
+      a_s();
+    }, 6300);
+    setTimeout(function () {
+      c();
+    }, 6700);
+    setTimeout(function () {
+      c();
+    }, 7200);
+    setTimeout(function () {
+      a_s();
+    }, 7500);
+    setTimeout(function () {
+      a();
+    }, 7800);
+    setTimeout(function () {
+      a_s();
+    }, 8100);
+    setTimeout(function () {
+      a();
+    }, 8400);
+    setTimeout(function () {
+      a_s();
+    }, 8700);
+  
+}
+
+if ( window.screen.availWidth < 600 ) {
+    alert("please rotate your phone to get the best experience")
+}
